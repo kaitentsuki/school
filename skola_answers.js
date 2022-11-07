cca1937c32f622bfde443399a8a930e7ef33d338
lukaskalny <lukas.kalny@designeo.cz>
2022-11-07 20:51:30 +0100

    course kit request answers listener, SW development answers

--- skola_answers.js
+++ skola_answers.js
@@ -1,77 +1,97 @@
-(function() {
+(function () {
     'use strict';
 
+    const lessons = [];
+
     const interceptNetworkRequests = (ee) => {
-    const open = XMLHttpRequest.prototype.open;
-    const send = XMLHttpRequest.prototype.send;
+        const open = XMLHttpRequest.prototype.open;
+        const send = XMLHttpRequest.prototype.send;
 
-    const isRegularXHR = open.toString().indexOf('native code') !== -1;
+        const isRegularXHR = open.toString().indexOf('native code') !== -1;
 
 
-    // don't hijack if already hijacked - this will mess up with frameworks like Angular with zones
-    // we work if we load first there which we can.
-    if (isRegularXHR) {
-        console.log('jsem xhr');
-        XMLHttpRequest.prototype.open = function() {
-            ee.onOpen && ee.onOpen(this, arguments);
-            if (ee.onLoad) {
-                this.addEventListener('load', ee.onLoad.bind(ee));
-            }
-            if (ee.onError) {
-                this.addEventListener('error', ee.onError.bind(ee));
-            }
-            return open.apply(this, arguments);
-        };
-        XMLHttpRequest.prototype.send = function() {
-            ee.onSend && ee.onSend(this, arguments);
-            return send.apply(this, arguments);
-        };
+        if (isRegularXHR) {
+            console.log('jsem xhr');
+            XMLHttpRequest.prototype.open = function () {
+                ee.onOpen && ee.onOpen(this, arguments);
+                if (ee.onLoad) {
+                    this.addEventListener('load', ee.onLoad.bind(ee));
+                }
+                if (ee.onError) {
+                    this.addEventListener('error', ee.onError.bind(ee));
+                }
+                return open.apply(this, arguments);
+            };
+            XMLHttpRequest.prototype.send = function () {
+                ee.onSend && ee.onSend(this, arguments);
+                return send.apply(this, arguments);
+            };
+        }
+
+        const fetch = window.fetch || "";
+
+        const isFetchNative = fetch.toString().indexOf('native code') !== -1;
+        if (isFetchNative) {
+            console.log('jsem fetch');
+            window.fetch = function () {
+                ee.onFetch && ee.onFetch(arguments);
+                const p = fetch.apply(this, arguments);
+                p.then(ee.onFetchResponse, ee.onFetchError);
+                return p;
+            };
+            // at the moment, we don't listen to streams which are likely video
+            const json = Response.prototype.json;
+            const text = Response.prototype.text;
+            const blob = Response.prototype.blob;
+            Response.prototype.json = function () {
+                const p = json.apply(this, arguments);
+                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "json"));
+                return p;
+            };
+            Response.prototype.text = function () {
+                const p = text.apply(this, arguments);
+                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "text"));
+                return p;
+            };
+            Response.prototype.blob = function () {
+                const p = blob.apply(this, arguments);
+                p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "blob"));
+                return p;
+            };
+        }
+        return ee;
     }
 
-    const fetch = window.fetch || "";
-    // don't hijack twice, if fetch is built with XHR no need to decorate, if already hijacked
-    // then this is dangerous and we opt out
-    const isFetchNative = fetch.toString().indexOf('native code') !== -1;
-    if(isFetchNative) {
-        console.log('jsem fetch');
-        window.fetch = function () {
-            ee.onFetch && ee.onFetch(arguments);
-            const p = fetch.apply(this, arguments);
-            p.then(ee.onFetchResponse, ee.onFetchError);
-            return p;
-        };
-        // at the moment, we don't listen to streams which are likely video
-        const json = Response.prototype.json;
-        const text = Response.prototype.text;
-        const blob = Response.prototype.blob;
-        Response.prototype.json = function () {
-            const p = json.apply(this, arguments);
-            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "json"));
-            return p;
-        };
-        Response.prototype.text = function () {
-            const p = text.apply(this, arguments);
-            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "text"));
-            return p;
-        };
-        Response.prototype.blob = function () {
-            const p = blob.apply(this, arguments);
-            p.then(ee.onFetchLoad && ee.onFetchLoad.bind(ee, "blob"));
-            return p;
-        };
-    }
-    return ee;
-}
+    const textToClipboard = (text) => {
+        var dummy = document.createElement("textarea");
+        document.body.appendChild(dummy);
+        dummy.value = text;
+        dummy.select();
+        document.execCommand("copy");
+        document.body.removeChild(dummy);
+    };
 
-interceptNetworkRequests({
-    onLoad: console.log,
-    onFetch: console.log,
-    onFetchResponse: console.log,
-    onFetchLoad: console.log,    
-    onOpen: console.log,
-    onSend: console.log,
-    onError: console.log,
-    onLoad: console.log
-});
+    document.addEventListener("keypress", function(e) {
+        if (e.code === "KeyJ") {
+            textToClipboard(JSON.stringify(lessons, null, 2));
+        }
+    });
+
+    interceptNetworkRequests({
+        // onLoad: (val) => console.log('load',val),
+        // onFetch:(val) => console.log('load',val),
+        // onFetchResponse: (val) => console.log('load',val),
+        onFetchLoad: (type, response) => {
+            if (typeof response === 'string') {
+               if('lesson' in JSON.parse(response)) {
+                    lessons.push(JSON.parse(response));
+               }
+            }
+        },
+        // onOpen: console.log,
+        // onSend: console.log,
+        // onError: console.log,
+        // onLoad: (val) => console.log('load',val),
+    });
 
 })();
